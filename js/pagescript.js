23b1f6d7bd7093d7e7a0fc01b316484f0b533e75
greenjew <grihan45@gmail.com>
2018-05-27 22:07:13 +0300

    Partners+authors
    сделал ещё 2 плашки и анимацию

--- js/pagescript.js
+++ js/pagescript.js
@@ -1,36 +1,8 @@
-
-$(document).ready(function () {
-
-
-    $(".").click(
-        //по клику открываем информацию
-        function showIinfo() {
-            var src = $(this).children(".thumbnail").attr('src');
-            var name = $(this).children(".event_title").text();
-            $("#timeline").prepend(
-                "<div class='zoomer'>" +
-                "<h1 class='zoomed_title' >" + name + "</h1>" +
-                "<img class='zoomed_img' src='" + src + "'/>" +
-                "<p class='zoomed_description'>" + + "</p>" +
-                "</div>");
-            //после картинки добавляем персонажей
-            $(".zoomer").append(
-                "<div class='grid'>" +
-                "<div class='col-1-1'>" +
-                "<div class='characters'>" +
-                "<h1 class='zoomed_title' style='margin-top: 10px'> Characters </h1>" +
-                "</div></div></div>");
-
-            $(".zoomer").fadeIn(1000);
-            $(".zoomer").click(function () {
-                $(".zoomer").fadeOut(1000);
-                setTimeout(function () {
-                    $(".zoomed_title").remove();
-                    $(".zoomed_description").remove();
-                    $(".characters").remove();
-                    $(".zoomer").remove()
-                }, 100)
-            });
-        });
-})
-;
+$(document).ready(function() {
+    $('a[href^="#"]').click(function(){
+        var el = $(this).attr('href');
+        $('body').animate({
+            scrollTop: $(el).offset().top}, 2000);
+        return false;
+    });
+});
