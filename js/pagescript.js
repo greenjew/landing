5b6ad3d13d64e5340157d17994a485ceaff8ae5e
greenjew <grihan45@gmail.com>
2018-06-12 14:51:01 +0300

    1.0

--- js/pagescript.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('a[href^="#"]').click(function(){
-        var el = $(this).attr('href');
-        $('body').animate({
-            scrollTop: $(el).offset().top}, 2000);
-        return false;
-    });
-});
